7c815178c41008c3858282ceedeafd535e868d1d
chengine <chengine@stanford.edu>
2024-09-17 11:59:28 -0700

    updated

--- remove_audio.py
+++ remove_audio.py
@@ -1,7 +1,7 @@
 #%%
 from moviepy.editor import VideoFileClip
 
-videoclip = VideoFileClip("static/videos/trial4_trim.mov")
+videoclip = VideoFileClip("static/videos/trial2_clip.mov")
 new_clip = videoclip.without_audio()
-new_clip.write_videofile("static/videos/trial4_trim.mov", codec="libx264", audio_codec="aac")
+new_clip.write_videofile("static/videos/trial2_clip.mov", codec="libx264", audio_codec="aac")
 # %%
